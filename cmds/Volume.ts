73852f330ad79987e27fbf960a78cea927073222
Marie <robloxfilmcam@gmail.com>
2020-12-26 01:57:25 +0100

    Update and rename Volume.ff to Volume.ts

---
+++ cmds/Volume.ts
@@ -0,0 +1,15 @@
+import { rad } from "../start"
+import * as Discord from "discord.js"
+
+module.exports = {
+    name: 'vol',
+    description: 'Change Volume',
+    category: 'General',
+    hide: false,
+    perms: [],
+    args: true,
+    usage: 'Please provide the volume 5-100',
+    execute(message, args) {
+	
+	}
+}
