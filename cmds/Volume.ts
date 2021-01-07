842201f9ecc0c2c9782e9be951f01a6e454d2ffe
Marie <robloxfilmcam@gmail.com>
2021-01-07 01:57:09 +0100

    Add functionality to script(no message output yet)

--- cmds/Volume.ts
+++ cmds/Volume.ts
@@ -1,5 +1,4 @@
 import { rad } from "../start"
-import * as Discord from "discord.js"
 
 module.exports = {
     name: 'vol',
@@ -8,8 +7,19 @@ module.exports = {
     hide: false,
     perms: [],
     args: true,
-    usage: 'Please provide the volume 5-100',
+    usage: 'Please provide the volume 10-100',
     execute(message, args) {
-      if(!rad.playing.find(guild => guild.id == message.guild.id)) return message.channel.send(`I'm not playing in this guild so volume will not be changed.`)
+        //@ts-ignore
+        if(!rad.playing.find(guild => guild.id == message.guild.id)) return message.channel.send(`I'm not playing in this guild so volume will not be changed.`)
+
+        const connection = rad.voice.connections.find(vc => vc.channel.id == message.member.voice.channelID)
+        if(!connection) return message.channel.send(`You're not in the VC`)
+
+        let volume = parseInt(args[0])
+
+        if(isNaN(volume)) return message.channel.send(`You did not provide a number`)
+        if(volume < 10 || volume > 100) return message.channel.send(`You provided a number out of the range! Please do a number between 10-100`)
+        
+        connection.dispatcher.setVolume(volume / 100.0)
     }
 }
