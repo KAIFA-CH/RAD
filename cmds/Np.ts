c7fbc444127bc9c1d359dbc10da32c6e55dad003
Marie <robloxfilmcam@gmail.com>
2021-01-08 04:00:11 +0100

    Remove ts-ignore

--- cmds/Np.ts
+++ cmds/Np.ts
@@ -10,12 +10,10 @@ module.exports = {
     perms: [],
     args: false,
     async execute(message, args) {
-        //@ts-ignore
         const playing = rad.playing.find(guild => guild.id === message.guild.id)
 
         if (!playing) return message.channel.send('I\'m not playing in this guild.')
 
-        //@ts-ignore
         const station = rad.radios.find(station => station.name.toLowerCase() == playing.station)
 
         const fetch = await axios.get(`https://api.radbot.tech/${station.name}`)
@@ -27,7 +25,6 @@ module.exports = {
             .setAuthor(`${rad.user.username} | Prefix: ${process.env.prefix}`, rad.user.displayAvatarURL())
             .setDescription(`Currently Playing: **${name}** \n Current Volume: **${message.guild.voice.connection.dispatcher.volume * 100.0}%**`)
             .setThumbnail(station.logo)
-            //@ts-ignore
             .setFooter(`Playback was started by ${message.guild.members.cache.get(playing.requested).displayName}`, message.guild.members.cache.get(playing.requested).user.avatarURL())
 
         message.channel.send({ embed })
