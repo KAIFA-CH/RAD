e8fddb26cd6a8ce8930596fabe275d833217707e
Marie <robloxfilmcam@gmail.com>
2021-08-09 09:29:41 +0200

    remove top.gg autoposter

--- start.ts
+++ start.ts
@@ -1,12 +1,8 @@
 import { ShardingManager } from "discord.js"
-import * as AutoPoster from "topgg-autoposter"
 import * as dotenv from "dotenv"
 dotenv.config()
 
 const manager = new ShardingManager('./bot.js', { token: process.env.key })
-
-//@ts-ignore
-const poster = AutoPoster('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4NTQwODI4MDY3OTY3Nzk5MyIsImJvdCI6dHJ1ZSwiaWF0IjoxNTg2NTcwNTg4fQ.fiHJWNnz4uPusuUt8BRiDoQVDOwmt20Jz9j0ArwVE-k', manager)
 
 manager.on('shardCreate', shard => {
     console.log(`Launched shard ${shard.id}`)
